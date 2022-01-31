5c1ad5a3db6d215391f1eeaeb63282ebe9c7ec72
Bhaskar <bhaskarraglya958@gmail.com>
2022-01-31 18:24:22 +0530

    improve suggestion

--- Triangle.java
+++ Triangle.java
@@ -2,38 +2,32 @@ package assignment_second;
 
 public class Triangle {
 
-	public double area(int base, int height)throws Exception {
-		// TODO Auto-generated method stub
-		if(base>=0 && height>=0) {
-		int ans =(base*height)/2;
-		
-		
-		return ans;
-	}else {
-		throw new Exception ("  Value should be greater then 0 it is postive");
+	public double area(int base, int height) throws Exception {
+		if (base >= 0 && height >= 0) {
+			int ans = base * height / 2;
+			return ans;
+		} else {
+			throw new Exception("Value should be greater then 0 it is postive");
+		}
 	}
 
-	}
-	public int perimeter(int one, int second, int third)throws Exception {
+	public int perimeter(int one, int second, int third) throws Exception {
 		// TODO Auto-generated method stub
-		if(one>=0 && second>=0 && third>=0) {
-		int ans=one +second+third;
-		
-		return ans;
-	}else {
-		throw new Exception ("  Value should be greater then 0 it is postive");
-	}
-	}
-
-	public double  volume(int base, int height, int lenght)throws Exception {
-		// TODO Auto-generated method stub
-		if(base>=0 && height>=0 && lenght>=0) {
-		double ans=(lenght*base*height)/2;
-		return ans;
-	}
-		else {
+		if (one >= 0 && second >= 0 && third >= 0) {
+			int ans = one + second + third;
+			return ans;
+		} else {
 			throw new Exception("  Value should be greater then 0 it is postive");
 		}
-	}	
+	}
 
+	public double volume(int base, int height, int lenght) throws Exception {
+		// TODO Auto-generated method stub
+		if (base >= 0 && height >= 0 && lenght >= 0) {
+			double ans = (lenght * base * height) / 2;
+			return ans;
+		} else {
+			throw new Exception("  Value should be greater then 0 it is postive");
+		}
+	}
 }
